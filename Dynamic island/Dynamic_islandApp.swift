7172d0567c9f4126734139ebd654ab8608ae0ae0
Prateek Bhardwaj <prateekbh111@gmail.com>
2026-04-29 15:32:30 +0530

    Initial Commit

---
+++ Dynamic island/Dynamic_islandApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dynamic_islandApp.swift
+//  Dynamic island
+//
+//  Created by Prateek Bhardwaj on 29/04/26.
+//
+
+import SwiftUI
+
+@main
+struct Dynamic_islandApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
