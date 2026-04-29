9893a4c4bb340e889ae99aadaeda5e92232bc066
Prateek Bhardwaj <prateekbh111@gmail.com>
2026-04-29 16:43:02 +0530

    feat: define MediaRemoteBridge protocol and Command enum

---
+++ DynamicIslandCore/Sources/DynamicIslandCore/MediaRemoteBridge.swift
@@ -0,0 +1,31 @@
+import Foundation
+
+public enum MediaCommand: Int, Sendable {
+    case play = 0
+    case pause = 1
+    case togglePlayPause = 2
+    case next = 4
+    case previous = 5
+    case toggleShuffle = 26
+}
+
+public struct NowPlayingSnapshot: Equatable, Sendable {
+    public let track: Track?
+    public let isPlaying: Bool
+    public let elapsed: TimeInterval
+
+    public init(track: Track?, isPlaying: Bool, elapsed: TimeInterval) {
+        self.track = track
+        self.isPlaying = isPlaying
+        self.elapsed = elapsed
+    }
+
+    public static let empty = NowPlayingSnapshot(track: nil, isPlaying: false, elapsed: 0)
+}
+
+public protocol MediaRemoteBridge: AnyObject, Sendable {
+    var onChange: (@Sendable (NowPlayingSnapshot) -> Void)? { get set }
+    func start()
+    func stop()
+    func send(_ command: MediaCommand)
+}
